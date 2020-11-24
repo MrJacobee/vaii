1d80a3ac96389ff0331d9b9f7a87e243e27bdeca
MrJacobee <66421183+MrJacobee@users.noreply.github.com>
2020-11-24 15:46:26 +0100

    Update cfg.php

--- admin/cfg.php
+++ admin/cfg.php
@@ -6,7 +6,7 @@ $databaza = "uni.kramar.im";
 $dtz = new DateTimeZone("Europe/Bratislava");
 $now = new DateTime('now', $dtz);
 
-$conn = mysqli_connect("localhost:3075","kramar.im","{}p[tgFy7!1dJkDFIKuvxZXyM\#D,b-Dn","uni.kramar.im");
+$conn = mysqli_connect("localhost:3075","kramar.im","","uni.kramar.im");
 
 if($conn === false){
     die( $now->format("d-m-Y H:i:s") . " - ERROR: Could not connect. " . mysqli_connect_error());
